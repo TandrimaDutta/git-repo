01826fd35ad45108ac344f2ba827ee64dac31ed4
HP <HP@192.168.0.105>
2020-10-16 09:27:38 +0530

    added prod class through b1

---
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -0,0 +1,25 @@
+package com.lti.model;
+
+public class Product {
+	int prodId;
+	String prodName;
+	public int getProdId() {
+		return prodId;
+	}
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public Product(int prodId, String prodName) {
+		super();
+		this.prodId = prodId;
+		this.prodName = prodName;
+	}
+	
+
+}
